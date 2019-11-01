0bf2d7e1c10663c595aebcaa9d58b78688dd888e
Yasuhiro Kojima <tiisaishima@gmail.com>
2019-11-02 07:35:16 +0900

    debug python dependencies

--- R/zzz.R
+++ R/zzz.R
@@ -12,7 +12,7 @@ safe_import_from_path <- function(module.name, libname, pckgname){
   packageStartupMessage("Import python module in this repository") 
   # reticulate::use_condaenv("r-reticulate")
   # reticulate::use_virtualenv("r-reticulate")
-  reticulate::py_install(c("numpy", "GPy"))
+  reticulate::py_install(c("numpy", "GPy", "matplotlib"))
   importlib <<- reticulate::import("importlib", delay_load = TRUE)
   py_builtin <<- reticulate::import_builtins()
   numpy <<- reticulate::import("numpy", delay_load = TRUE)
